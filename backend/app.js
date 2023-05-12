9774ad0b591674c01dc574ab6c8f4d61fb347e85
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-05-12 19:01:55 +0530

    refector : socket io addded into server.js

--- backend/app.js
+++ backend/app.js
@@ -39,4 +39,52 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
+// socket.io connection
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+  // join room
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  // typing message
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  // stop typing message
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  // send message
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+    // if there is no chat then return
+    if (!chat.users) return console.log("chat.users not defined");
+    // loop through all users in chat and send message to each of them individually
+    chat.users.forEach((user) => {
+      // if user is not sender then send message to that user  else return 
+      if (user._id == newMessageRecieved.sender._id) return; // if user is sender then return 
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  // when user disconnect from socket then leave the room and disconnect the socket connection  
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
