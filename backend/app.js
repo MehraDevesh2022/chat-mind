107c6a31567f49472086082e65ece194a9030f76
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-05-17 22:30:11 +0530

    chore : vercel.json added

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ cloudinary.config({
 
 // -----deployement code-----
 
-console.log(process.env.NODE_ENV);
+const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
