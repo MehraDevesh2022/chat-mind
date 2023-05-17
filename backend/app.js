05385088e7aea8e97a9a3095574e7d1e18076300
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-05-18 00:51:29 +0530

    refector : deployement code added

--- backend/app.js
+++ backend/app.js
@@ -41,18 +41,18 @@ cloudinary.config({
 
 // -----deployement code-----
 
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // connect to DB
 connectDB();
